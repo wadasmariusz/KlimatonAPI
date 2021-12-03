44e39f411341a4c6ebd37ff8c711e8bb8d7e01aa
Kamil Pisz <kamilpisz123@outlook.com>
2021-12-03 20:44:47 +0100

    add Sensor Models

---
+++ ThreatMap.Domain/Sensors/Entities/SensorM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreatMap.Domain.Common.Entities;
+using ThreatMap.Domain.Sensors.Enums;
+
+namespace ThreatMap.Domain.Sensors.Entities
+{
+    public class SensorM : AuditableEntity
+    {
+        public string Name { get; set; }
+        public SensorCategoryE Category { get; set; }
+        public string Description { get; set; }
+
+
+        //Dane Lokalizacyjne = tutaj w double albo w geospatial type z Postgisa
+        public double? LocLat { get; set; }
+        public double? LocLon { get; set; }
+        public double? LocAlt { get; set; }
+
+
+    }
+}
