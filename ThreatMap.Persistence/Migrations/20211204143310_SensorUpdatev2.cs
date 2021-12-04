e7ddb79bf7ba94f4644bab8ab62b4ee5ecb4187e
Kamil Grochmal <kamil.grochmal@gmail.com>
2021-12-04 15:33:57 +0100

    SensorUpdateV2 Migration

---
+++ ThreatMap.Persistence/Migrations/20211204143310_SensorUpdatev2.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThreatMap.Persistence.Migrations
+{
+    public partial class SensorUpdatev2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Institutions");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Sensors",
+                newName: "ZipCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Sensors",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "LocationId",
+                table: "Sensors",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                table: "Sensors",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<short>(
+                name: "Source",
+                table: "Sensors",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Sensors",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Sensors");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Sensors");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Sensors");
+
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "Sensors");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Sensors");
+
+            migrationBuilder.RenameColumn(
+                name: "ZipCode",
+                table: "Sensors",
+                newName: "Address");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Institutions",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
