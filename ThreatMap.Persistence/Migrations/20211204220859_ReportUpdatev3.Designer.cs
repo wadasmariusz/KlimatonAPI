af20bf9b37b43f51ab1e0be97113bbb46d4f7bfc
Kamil Grochmal <kamil.grochmal@gmail.com>
2021-12-04 23:09:18 +0100

    Add ReportUpdateV3 migration

--- ThreatMap.Persistence/Migrations/20211204220859_ReportUpdatev3.Designer.cs
+++ ThreatMap.Persistence/Migrations/20211204220859_ReportUpdatev3.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ThreatMap.Persistence;
@@ -11,9 +12,10 @@ using ThreatMap.Persistence;
 namespace ThreatMap.Persistence.Migrations
 {
     [DbContext(typeof(ThreatMapDbContext))]
-    partial class ThreatMapDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211204220859_ReportUpdatev3")]
+    partial class ReportUpdatev3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ThreatMap.Persistence/Migrations/20211204220859_ReportUpdatev3.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThreatMap.Persistence.Migrations
+{
+    public partial class ReportUpdatev3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Altitude",
+                table: "Reports",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Reports",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Reports",
+                type: "double precision",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Altitude",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Reports");
+        }
+    }
+}
